a51879218d5b19ef8ba350698a47cbdd77c10a5a
Vikas Shil <shil.vikas@gmail.com>
2018-02-18 01:28:33 +0530

    ongoing changes

--- plivo/urls.py
+++ plivo/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -22,3 +24,5 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^contact/', include('contact.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
